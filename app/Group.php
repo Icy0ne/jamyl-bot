d51b77e2226e95deceb64e679c7d11bdf3f75530
Icy0ne <me@icyone.net>
2015-06-12 13:45:21 +0100

    Add Alliance, Contact and Corp models Add commands to populate models

--- app/Group.php
+++ app/Group.php
@@ -1,4 +1,6 @@
-<?php namespace JamylBot;
+<?php
+
+namespace JamylBot;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -6,59 +8,63 @@ class Group extends Model {
 
 	protected $fillable = ['name', 'owners'];
 
-    public function users()
-    {
-        return $this->belongsToMany('JamylBot\User');
-    }
+	public function users()
+	{
+		return $this->belongsToMany('JamylBot\User');
+	}
 
-    public function channels()
-    {
-        return $this->belongsToMany('JamylBot\Channel');
-    }
+	public function channels()
+	{
+		return $this->belongsToMany('JamylBot\Channel');
+	}
 
-    public function getOwners()
-    {
-        return explode(',', $this->owners);
-    }
+	public function getOwners()
+	{
+		return explode(',', $this->owners);
+	}
 
-    public function setOwners($ownersArray)
-    {
-        $this->owners = implode(',', $ownersArray);
-        $this->save();
-    }
+	public function setOwners($ownersArray)
+	{
+		$this->owners = implode(',', $ownersArray);
+		$this->save();
+	}
 
-    /**
-     * @param int $newOwner
-     */
-    public function addOwner($newOwner)
-    {
-        $owners = $this->getOwners();
-        $owners[] = $newOwner;
-        $this->owners = implode(',', array_unique($owners));
-        $this->save();
-    }
+	/**
+	 *
+	 * @param int $newOwner        	
+	 */
+	public function addOwner($newOwner)
+	{
+		$owners = $this->getOwners();
+		$owners[] = $newOwner;
+		$this->owners = implode(',', array_unique($owners));
+		$this->save();
+	}
 
-    public function removeOwner($owner)
-    {
-        $owners = $this->getOwners();
-        $owners = array_diff($owners, [$owner]);
-        $this->setOwners($owners);
-    }
+	public function removeOwner($owner)
+	{
+		$owners = $this->getOwners();
+		$owners = array_diff($owners, [$owner]);
+		$this->setOwners($owners);
+	}
 
-    public function isOwner($owner)
-    {
-        return in_array($owner, $this->getOwners());
-    }
+	public function isOwner($owner)
+	{
+		return in_array($owner, $this->getOwners());
+	}
 
-    public function isMemberBySlack($slack_id)
-    {
-        foreach ($this->users as $user){
-            /** @var User $user */
-            if ($user->slack_id != null && $user->slack_id == $slack_id && $user->hasAccess()) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public function isMemberBySlack($slack_id)
+	{
+		foreach ($this->users as $user) {
+			/**
+			 *
+			 * @var User $user
+			 */
+			if ($user->slack_id != null && $user->slack_id == $slack_id && $user->hasAccess()) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
