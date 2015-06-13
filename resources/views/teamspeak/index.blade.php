ef923223e434f2194e35079d80e532217ceafa42
Icy0ne <me@icyone.net>
2015-06-13 02:28:51 +0100

    Bootstrapped form controls

--- resources/views/teamspeak/index.blade.php
+++ resources/views/teamspeak/index.blade.php
@@ -14,7 +14,12 @@
 		                    <div class="col-sm-3 text-right">
 		                    	<strong>TeamSpeak Address</strong>
 		                    </div>
-		                    <div class="col-sm-7"><a href="ts3server://{{ config('ts3.hostname') }}?port={{ config('ts3.serverport') }}&nickname={{ $user }}">{{ config('ts3.hostname') }}</a> (click to join)</div>
+		                    <div class="col-sm-4">
+		                    	<a href="ts3server://{{ config('ts3.hostname') }}?port={{ config('ts3.serverport') }}&nickname={{ $user }}">{{ config('ts3.hostname') }}</a>
+	                    	</div>
+		                    <div class="col-sm-5">
+		                    	<a class="btn btn-success btn-sm col-sm-4" href="ts3server://{{ config('ts3.hostname') }}?port={{ config('ts3.serverport') }}&nickname={{ $user }}">Click To Join</a>
+	                    	</div>
 						</div>
 						<div class="row">
 		                    <div class="col-sm-3 text-right">
@@ -30,21 +35,21 @@
 		                    <div class="col-sm-5">
 								@if ($tsuid!="")
 			                 		{!! Form::open(['action' => ['TeamSpeakController@register'], 'method' => 'post']) !!}
-				                    {!! Form::submit('Refresh TS UID') !!}
+				                    {!! Form::submit('Refresh TS UID', Array('class'=>'btn btn-warning btn-sm col-sm-4')) !!}
 			                        {!! Form::close() !!}
 		                        @endif
 		                    </div>
                     	</div>	
 						<div class="row">
-		                    <div class="col-sm-offset-3 col-sm-2">
+		                    <div class="col-sm-offset-3 col-sm-6">
 								@if ($tsuid=="")
 		    	                    {!! Form::open(['action' => ['TeamSpeakController@register'], 'method' => 'post']) !!}
-		                            {!! Form::submit('Register') !!}
+		                            {!! Form::submit('Register', Array('class'=>'btn btn-success btn-sm col-sm-4')) !!}
 			                        {!! Form::close() !!}
 		                        @endif
 								@if ($tsuid!="")
 				                    {!! Form::open(['action' => ['TeamSpeakController@updateAccess'], 'method' => 'post']) !!}
-		                            {!! Form::submit('Update Access') !!}
+		                            {!! Form::submit('Force Access Update', Array('class'=>'btn btn-danger btn-sm col-sm-6')) !!}
 			                        {!! Form::close() !!}
 		                        @endif
 	                        </div>
